79f4641f50f9e178096f65eac1b656e84523994c
kdh5790 <k_0106@naver.com>
2025-02-18 19:13:35 +0900

    [ Feat ] LobbyScene 상점 닫기 버튼/기능 추가
    닫기 버튼 클릭 시 상점 UI 닫히도록 버튼 추가 및 ShopUI.cs 변경

--- Assets/Scripts/LobbyScene/UI/ShopUI.cs
+++ Assets/Scripts/LobbyScene/UI/ShopUI.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopUI : MonoBehaviour
 {
+    Button closeButton;
+
+    private void Start()
+    {
+        closeButton = transform.Find("CloseButton").GetComponent<Button>();
+        closeButton.onClick.AddListener(CloseShopUI);
+    }
+
     public void OpenShopUI()
     {
         gameObject.SetActive(true);
