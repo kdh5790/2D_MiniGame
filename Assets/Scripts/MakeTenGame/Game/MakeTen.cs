1dd7c2d5fa47dbea3a194bc1b803bbdb08ab16a0
kdh5790 <k_0106@naver.com>
2025-02-19 22:17:01 +0900

    [ Feat ] MakeTenScene 게임종료 시 골드 획득 추가
    게임종료 시 GoldManager에 골드 쌓이도록 코드 추가

--- Assets/Scripts/MakeTenGame/Game/MakeTen.cs
+++ Assets/Scripts/MakeTenGame/Game/MakeTen.cs
@@ -25,15 +25,19 @@ public class MakeTen : MonoBehaviour
 
     private void Update()
     {
-        if(time > 0)
+        if (time > 0)
         {
             time -= Time.deltaTime;
         }
 
-        if(time < 0)
+        if (time < 0)
         {
             time = 0;
             isDead = true;
+
+            if (GoldManager.instance != null)
+                GoldManager.instance.MiniGameGold += score;
+
             FindObjectOfType<MakeTenGameUI>().gameObject.SetActive(false);
             FindObjectOfType<MakeTenScoreUI>(true).gameObject.SetActive(true);
         }
