914e588730a9add0cb4c56fd01b24326fb867d57
kdh5790 <k_0106@naver.com>
2025-02-19 12:11:55 +0900

    [ Feat ] ObstaclesScene 백그라운드 완성
    ObstaclesScene 무한 백그라운드 완성

--- Assets/Scripts/AvoidingObstaclesScene/ObstaclesBackGround.cs
+++ Assets/Scripts/AvoidingObstaclesScene/ObstaclesBackGround.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class ObstaclesBackGround : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<GameObject> backgroundList;
+    public float speed = 2f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.down * Time.deltaTime * speed);
     }
 }
---
+++ Assets/Scripts/AvoidingObstaclesScene/ObstaclesCollider.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesCollider : MonoBehaviour
+{
+    private readonly float fixedValue = 10.24f; // 백그라운드 오브젝트 간의 거리
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == null) return;
+
+        // 부딪힌 오브젝트 태그가 백그라운드라면
+        if (collision.gameObject.CompareTag("BackGround"))
+        {
+            // 부모 오브젝트에서 Background 클래스 가져오기
+            ObstaclesBackGround bg = collision.gameObject.GetComponentInParent<ObstaclesBackGround>();
+
+            if (bg == null) { Debug.Log("Background를 찾지 못했습니다."); return; }
+
+
+            float distance = 0f; // 거리를 저장할 변수
+            Vector3 postion = new Vector3(); // 위치를 저장할 변수
+
+            foreach (var image in bg.backgroundList)
+            {
+                // 가장 먼 거리 구하기
+                if (Vector3.Distance(collision.gameObject.transform.position, image.transform.position) > distance)
+                {
+                    distance = Vector3.Distance(collision.gameObject.transform.position, image.transform.position);
+                    postion = new Vector3(0, image.transform.position.y + fixedValue, 0);
+                }
+            }
+
+            // 가장 먼거리의 오브젝트의 (x값 + 고정값, 0, 0) 으로 위치 변경
+            collision.gameObject.transform.position = postion;
+        }
+    }
+}
