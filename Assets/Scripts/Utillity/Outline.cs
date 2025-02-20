9b4e8c4d5e0c0290723e4a77009e996bfb5c3f90
kdh5790 <k_0106@naver.com>
2025-02-20 10:28:39 +0900

    [ Comment ] Utillity 폴더 스크립트 주석 추가
    Outline.cs, ScreenFader.cs 주석 추가

--- Assets/Scripts/Utillity/Outline.cs
+++ Assets/Scripts/Utillity/Outline.cs
@@ -15,6 +15,7 @@ public class Outline : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // 트리거에 닿은 오브젝트의 태그가 Player라면 아웃라인 활성화
         if(collision.CompareTag("Player"))
         {
             sprite.material.SetFloat("_OutlineEnabled", 1);
@@ -25,6 +26,7 @@ public class Outline : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            // 트리거에서 나간 오브젝트의 태그가 Player라면 아웃라인 비활성화
             sprite.material.SetFloat("_OutlineEnabled", 0);
         }
     }
--- Assets/Scripts/Utillity/ScreenFader.cs
+++ Assets/Scripts/Utillity/ScreenFader.cs
@@ -37,6 +37,7 @@ public class ScreenFader : MonoBehaviour
 
     public IEnumerator FadeOutSceneChange(Scene state)
     {
+        // 현재 Scene에 존재하는 캔버스들 찾기
         canvas = FindObjectsOfType<Canvas>();
 
         // 캔버스 비활성화
@@ -52,25 +53,32 @@ public class ScreenFader : MonoBehaviour
         // 목표 컬러
         Color targetColor = new Color(0, 0, 0, 1);
 
+        // 시간
         float t = 0f;
 
+        // 1.5 : 페이드 아웃 할 시간
         while (t < 1.5f)
         {
             t += Time.deltaTime;
-            float clamp = Mathf.Clamp01(t / 1);
-            sprite.color = Color.Lerp(sprite.color, targetColor, clamp);
+            float clamp = Mathf.Clamp01(t / 1); // 값을 0에서 1로 제한
+            sprite.color = Color.Lerp(sprite.color, targetColor, clamp); // 현재 스프라이트 컬러를 tagetColor가 될 때 까지 clamp변수만큼 증가
 
             yield return null;
         }
 
+        // Scene 변경
         SceneManager.LoadScene((int)state);
+
+        // Scene 변경 이후 FadeIn 코루틴 시작
         StartCoroutine(FadeInSceneChange());
     }
 
     public IEnumerator FadeInSceneChange()
     {
+        // 스프라이트 검은색으로 변경
         sprite.color = new Color(0, 0, 0, 1);
 
+        // 현재 Scene에 존재하는 캔버스들 찾기
         canvas = FindObjectsOfType<Canvas>();
 
 
@@ -88,7 +96,7 @@ public class ScreenFader : MonoBehaviour
             yield return null;
         }
 
-        // 캔버스 활성화
+        // FadeIn 완료 후 모든 캔버스 활성화
         if (canvas != null)
         {
             foreach (Canvas c in canvas)
