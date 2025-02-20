a2538b4704e14536a7e848b8ee12ce1e25eb4662
kdh5790 <k_0106@naver.com>
2025-02-20 16:00:59 +0900

    [ perf ] ObstacleScene 장애물 생성 로직 변경
    파괴 후 생성 -> 오브젝트 풀링을 사용해 비활성화, 활성화 하는 방식으로 변경

--- Assets/Scripts/AvoidingObstaclesScene/Obstacle/ObstacleCreator.cs
+++ Assets/Scripts/AvoidingObstaclesScene/Obstacle/ObstacleCreator.cs
@@ -7,17 +7,34 @@ public class ObstacleCreator : MonoBehaviour
 {
     public GameObject obtaclePrefab;
     public List<GameObject> obtacleList = new List<GameObject>();
+    
+    private int poolCount = 5;
+    private Queue<GameObject> obstaclePool = new Queue<GameObject>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < poolCount; i++)
+        {
+            GameObject go = Instantiate(obtaclePrefab, transform);
+            go.SetActive(false);
+            obstaclePool.Enqueue(go);
+        }
+    }
 
     public void CreateObtacle()
     {
         // 현재 생성된 장애물을 파괴 후 다시 랜덤한 위치에 장애물 생성
         foreach(var obj in obtacleList)
-            Destroy(obj);
+        { 
+            obj.SetActive(false);
+            obstaclePool.Enqueue(obj);
+        }
 
         for (int i = 0; i < 5; i++)
         {
-            GameObject go = Instantiate(obtaclePrefab, transform);
+            GameObject go = obstaclePool.Dequeue();
             go.transform.localPosition = new Vector2(Random.Range(-1f, 1f), Random.Range(-1.3f, 1.3f));
+            go.SetActive(true);
             obtacleList.Add(go);
         }
     }
